0e7120b0edc6f8687437e7ac4ddbcb8bd0d78ec0
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-10-25 10:22:46 +1100

    learn sum of two random integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {return 'ERROR'}
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {return 'ERROR'}
 
+    let low = 0;
+    let high = 0;
+    
+    if (a < b) {
+        low = a;
+        high = b;
+    } else {
+        low = b;
+        high = a;
+    }
+
+    let total = 0;
+
+    for (i = low; i <= high; i++) {
+        total += i;
+    }
+
+    return total
 };
 
 // Do not edit below this line
