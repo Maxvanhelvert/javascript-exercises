778fdaffe54428476d9572e496e607744cf4abf4
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-10-25 10:45:13 +1100

    learn temp conversion logic

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let celcius = (far - 32) / (9/5);
+  let roundedC = Math.round(celcius * 10) / 10;
+  return roundedC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let farenheit = cel * (9/5) + 32; 
+  let roundedF = Math.round(farenheit * 10) / 10;
+  return roundedF
 };
 
 // Do not edit below this line
